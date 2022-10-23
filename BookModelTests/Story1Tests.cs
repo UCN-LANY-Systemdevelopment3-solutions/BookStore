bc51b79f45452b1b908775b3cf0921203aef6f6b
Lars Nysom <LANY@ucn.dk>
2022-10-23 13:37:50 +0200

    Add project files.

---
+++ BookModelTests/Story1Tests.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookModelTests
+{
+    public class Story1Tests
+    {
+        // 1. Code the test to validate only the specific requirements.
+        // 2. Verify that you can’t compile (because the code you want to test doesn’t exist yet).
+        // 3. Implement just enough code (a stub/empty class) to make the code compile.
+        // 4. Verify that your test fails
+        // 5. Only implement just enough code to make the test pass.
+        
+
+    }
+}
