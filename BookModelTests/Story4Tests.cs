a4ccb2fece8a0a357b18120f18af5e8113799f89
Lars Nysom <LANY@ucn.dk>
2022-10-24 13:44:13 +0200

    Added tests for Story 4

--- BookModelTests/Story4Tests.cs
+++ BookModelTests/Story4Tests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookShop.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,52 @@ using System.Threading.Tasks;
 
 namespace BookModelTests
 {
-    public class Story4Tests
+    public class Story4Tests : IClassFixture<BookStoreFixture>
     {
+        private readonly BookStoreFixture _bookStoreFixture;
+
+        public Story4Tests(BookStoreFixture bookStoreFixture)
+        {
+            _bookStoreFixture = bookStoreFixture;
+        }
+
+        [Fact]
+        public void TestPutBookIntoCart()
+        {
+            // Arrange
+            ShoppingCart cart = new();
+            Book book1 = _bookStoreFixture.Book1;
+            decimal expectedTotal = 299.00M;
+
+            // Act
+            cart.Add(book1);
+
+            // Assert
+            Assert.Single(cart.Orderlines);
+            Assert.Contains(book1, cart.Orderlines);
+            Assert.Equal(expectedTotal, cart.Total);
+        }
+
+
+        [Fact]
+        public void TestCalculateTotalsWhenAddingBookToCart()
+        {
+            // Arrange
+            ShoppingCart cart = new();
+            Book book1 = _bookStoreFixture.Book1;
+            Book book2 = _bookStoreFixture.Book2;
+            Book book3 = _bookStoreFixture.Book3;
+            decimal expectedTotal = 1146.00M;
+
+            // Act
+            cart.Add(book1);
+            cart.Add(book1);
+            cart.Add(book2);
+            cart.Add(book3);
+
+            // Assert
+            Assert.Equal(3, cart.Orderlines.Count());
+            Assert.Equal(expectedTotal, cart.Total);
+        }
     }
 }
---
+++ Model/ShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShop.Model
+{
+    public class ShoppingCart
+    {
+        public IEnumerable<Book> Orderlines { get; set; }
+        public decimal Total { get; set; }
+
+        public void Add(Book book1)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
